b2f67e89de55a381073b2dbd283b8053ab855f28
Syahuda Nahatmasuni <syahudanahatmasuni@gmail.com>
2022-06-04 14:11:40 +0700

    done

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, callbackSort) => 
+  callbackSort(names).map((name, i) => `${i + 1}. ${name}`);
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => data.sort();
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => data.sort().reverse();
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
